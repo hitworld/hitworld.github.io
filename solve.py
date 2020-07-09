b670d4fa9679583a317b48c96bdd56993f50c30c
w4rd3n <543546992@qq.com>
2020-07-09 11:20:58 +0800

    Site updated: 2020-07-09 11:20:25

--- solve.py
+++ solve.py
@@ -1,5 +1,5 @@
 import os
-path = "A:/Blog/source/_posts"
+path = "A:/blog-hexo/source/_posts"
 files= os.listdir(path)
 s = []
 for file in files:
@@ -10,15 +10,15 @@ for file in files:
             print("md is error: " + file)
         continue
     if not os.path.isdir(file):
-        if file[:-3] not in files:
-            print("dir is error: " + file)
+        # if file[:-3] not in files:
+            # print("dir is error: " + file)
         with open(path + "/" + file,'r', encoding='UTF-8') as r:
             lines = r.readlines()
         with open(path + "/" + file,'w', encoding='UTF-8') as w:
             flag = 0
             for l in lines:
-                # if 'abbrlink' in l:
-                #     continue
+                if 'abbrlink' in l:
+                    continue
                 if 'asset_img' in l:
                     if(not os.path.exists(path + "/" + file[:-3] + "/" + l[13:l.find("png") + 3])):
                         print(path + "/" + file[:-3] + "/" + l[13:l.find("png") + 3])
